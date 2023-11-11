03195c894214798a6d00338acf6be1fc1a52a480
barathrum54 <tahabdurmus0@gmail.com>
2023-11-11 18:27:07 +0300

    feat: Register form implemented

---
+++ src/components/Auth/Register/RegisterForm.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-card class="register-card">
+    <v-card-title class="text-center">Register Your Account</v-card-title>
+
+    <v-card-text>
+      <v-form @submit.prevent="handleSubmit" ref="form">
+        <v-text-field
+          v-model="email"
+          label="Email"
+          type="email"
+          required
+          autocomplete="false"
+          hint="Enter your password to access this website"
+          :rules="[rules.required, rules.email]"
+        ></v-text-field>
+
+        <v-text-field
+          v-model="password"
+          label="Password"
+          type="password"
+          required
+          autocomplete="false"
+          :rules="[rules.required, rules.password]"
+        ></v-text-field>
+
+        <v-text-field
+          v-model="repeatPassword"
+          label="Repeat Password"
+          type="password"
+          required
+          autocomplete="false"
+          :rules="[rules.required, rules.matchPassword]"
+        ></v-text-field>
+
+        <v-btn type="submit" color="primary" class="mt-4" :loading="isLoading">
+          Register
+        </v-btn>
+      </v-form>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+import { ref, Ref } from "vue";
+
+const rules = {
+  required: (value: any) => !!value || "Field is required",
+  email: (value: string) =>
+    /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value) || "Enter a valid email address",
+  password: (value: string) =>
+    (value && value.length >= 6) || "Password must be at least 6 characters",
+  matchPassword: (value: string) =>
+    value === password.value || "Passwords do not match",
+};
+
+const email: Ref<string> = ref("");
+const password: Ref<string> = ref("");
+const repeatPassword: Ref<string> = ref("");
+const isLoading: Ref<boolean> = ref(false);
+
+const handleSubmit = (): void => {
+  isLoading.value = true;
+  setTimeout(() => {
+    isLoading.value = false;
+    console.log("API request successful");
+  }, 2000);
+};
+</script>
+
+<style scoped lang="scss">
+.register-card {
+  max-width: 400px;
+  margin: auto;
+  padding: 20px;
+}
+</style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -7,44 +7,10 @@
 
       <v-row justify="center">
         <v-col cols="12" md="8" lg="6">
-          <v-card class="register-card">
-            <v-card-title class="text-center"
-              >These will be used as login details</v-card-title
-            >
-
-            <v-card-text>
-              <v-form @submit.prevent="registerUser">
-                <v-text-field
-                  v-model="username"
-                  label="Username"
-                  required
-                  autocomplete="false"
-                ></v-text-field>
-                <v-text-field
-                  v-model="email"
-                  label="Email"
-                  type="email"
-                  required
-                  autocomplete="false"
-                ></v-text-field>
-                <v-text-field
-                  v-model="password"
-                  label="Password"
-                  type="password"
-                  required
-                  autocomplete="false"
-                ></v-text-field>
-
-                <v-btn
-                  type="submit"
-                  color="primary"
-                  class="mt-4"
-                  :loading="isLoading"
-                  >Register</v-btn
-                >
-              </v-form>
-            </v-card-text>
-          </v-card>
+          <register-form
+            @submit-succes="registerFormSubmitSuccess"
+          ></register-form>
+          <ConfirmAccount/>
         </v-col>
       </v-row>
     </v-responsive>
@@ -52,24 +18,15 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import RegisterForm from "@/components/Auth/Register/RegisterForm.vue";
+import { onMounted } from "vue";
+import { ref, Ref } from "vue";
+const registerState: Ref<string> = ref("form");
 
-const username = ref("");
-const email = ref("");
-const password = ref("");
-const isLoading = ref(false);
-const registerUser = () => {
-  isLoading.value = true;
-  setTimeout(() => {
-    isLoading.value = false;
-  }, 2000);
+onMounted(() => {
+  registerState.value = "form";
+});
+const registerFormSubmitSuccess = () => {
+  registerState.value = "confirmPending";
 };
 </script>
-
-<style scoped lang="scss">
-.register-card {
-  max-width: 400px;
-  margin: auto;
-  padding: 20px;
-}
-</style>
